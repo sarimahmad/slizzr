0812bbde372b8195d7896ff47bfe8642a8a33958
Tayyab Jamil <tayyab.jamil@ivylabtech.com>
2021-07-05 15:13:45 +0500

    time issue

--- App/screens/manageEvents/index.js
+++ App/screens/manageEvents/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
 import {
   View,
   Text,
@@ -7,30 +8,27 @@ import {
   StyleSheet,
   FlatList,
 } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import { BLACK, BLUE, WHITE } from '../../helper/Color';
-import { FONT, SCREEN } from '../../helper/Constant';
+import {SafeAreaView} from 'react-navigation';
+import {BLACK, WHITE} from '../../helper/Color';
+import {FONT, SCREEN} from '../../helper/Constant';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import HeaderWithOptionBtn from '../../component/HeaderWithOptionBtn';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import firestore from '@react-native-firebase/firestore';
 import moment from 'moment';
-import {getUserAttendedEvents,getUserEvents} from '../../helper/Api'
-import {connect} from 'react-redux';
+import {getUserAttendedEvents, getUserEvents} from '../../helper/Api';
 import Loader from '../../component/Loader';
- class manageEvents extends Component {
+import {connect} from 'react-redux';
+class manageEvents extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
       attendingEvents: true,
       myevents: false,
+      loading: true,
       index: 1,
       userEvents: [],
-      loading:false,
       userAttendedEvents: [],
       messages: [
         {
@@ -73,76 +71,78 @@ import Loader from '../../component/Loader';
           adress: 'Host: Tallah Cotton',
           date: '11:30 PM | Feb 25, 2020 - WED',
         },
-
-      ]
-
+      ],
     };
   }
 
   componentDidMount() {
-    this.getUserEvents()
-    this.getUserAttendedEvents()
+    this.getUserEvents();
+    this.getUserAttendedEvents();
   }
 
   async getUserEvents() {
-    this.setState({loading:true})
-   
-    await getUserEvents(this.props.userToken).then((response) => {
-      this.setState({ userEvents: response.UserHostedEvent }) 
-      this.setState({loading:false})
-   
+    await getUserEvents(this.props.userToken).then(response => {
+      this.setState({userEvents: response.UserHostedEvent, loading: false});
     });
   }
 
-  async getUserAttendedEvents(){
-   this.setState({loading:true})
-      
-      await getUserAttendedEvents(this.props.userToken).then((response) => {
-        this.setState({ userAttendedEvents: response.UserAttendedEvents }) 
-        this.setState({loading:false})
-   
-      });
-    
+  async getUserAttendedEvents() {
+    await getUserAttendedEvents(this.props.userToken).then(response => {
+      this.setState({userAttendedEvents: response.UserAttendedEvents});
+    });
   }
 
-  barTapped = (indexTap) => {
+  barTapped = indexTap => {
     if (indexTap === 1) {
-      this.setState({ index: 1 })
+      this.setState({index: 1});
     } else if (indexTap === 2) {
-      this.setState({ index: 2 })
-    } if (indexTap === 3) {
-      this.setState({ index: 3 })
-    } if (indexTap === 4) {
-      this.setState({ index: 4 })
+      this.setState({index: 2});
     }
-  }
+    if (indexTap === 3) {
+      this.setState({index: 3});
+    }
+    if (indexTap === 4) {
+      this.setState({index: 4});
+    }
+  };
 
   myevents = () => {
-    this.setState({ myevents: true })
-    this.setState({ attendingEvents: false })
-  }
+    this.setState({myevents: true});
+    this.setState({attendingEvents: false});
+  };
   attendingEvents = () => {
-    this.setState({ myevents: false })
-    this.setState({ attendingEvents: true })
-
-  }
+    this.setState({myevents: false});
+    this.setState({attendingEvents: true});
+  };
   topBar = () => {
     return (
       <View style={styles.flexRow}>
-
         <TouchableOpacity
           style={
-            this.state.index == 1
-              ? { borderBottomColor: '#F818D9', borderBottomWidth: 3, borderColor: 'lightgrey', justifyContent: 'center', borderWidth: 1, width: SCREEN.width * 0.5, height: 39 }
-              : { color: 'black', width: SCREEN.width * 0.5, height: 39, borderColor: 'lightgrey', borderWidth: 1, justifyContent: 'center' }
+            this.state.index === 1
+              ? {
+                  borderBottomColor: '#F818D9',
+                  borderBottomWidth: 3,
+                  borderColor: 'lightgrey',
+                  justifyContent: 'center',
+                  borderWidth: 1,
+                  width: SCREEN.width * 0.5,
+                  height: 39,
+                }
+              : {
+                  color: 'black',
+                  width: SCREEN.width * 0.5,
+                  height: 39,
+                  borderColor: 'lightgrey',
+                  borderWidth: 1,
+                  justifyContent: 'center',
+                }
           }
           onPress={() => this.barTapped(1)}>
           <Text
             style={[
               styles.barText,
-              this.state.index == 1
-                ? { color: '#F818D9' }
-                : { color: 'black' },
+              this.state.index === 1 ? {color: '#F818D9'} : {color: 'black'},
             ]}>
             MY EVENTS
           </Text>
@@ -150,48 +150,60 @@ import Loader from '../../component/Loader';
 
         <TouchableOpacity
           style={
-            this.state.index == 2
-              ? { borderBottomColor: '#F818D9', borderBottomWidth: 3, borderColor: 'grey', justifyContent: 'center', borderWidth: 1, width: SCREEN.width * 0.5, height: 39 }
-              : { color: 'black', width: SCREEN.width * 0.5, height: 39, borderColor: 'grey', justifyContent: 'center', borderWidth: 1 }
+            this.state.index === 2
+              ? {
+                  borderBottomColor: '#F818D9',
+                  borderBottomWidth: 3,
+                  borderColor: 'grey',
+                  justifyContent: 'center',
+                  borderWidth: 1,
+                  width: SCREEN.width * 0.5,
+                  height: 39,
+                }
+              : {
+                  color: 'black',
+                  width: SCREEN.width * 0.5,
+                  height: 39,
+                  borderColor: 'grey',
+                  justifyContent: 'center',
+                  borderWidth: 1,
+                }
           }
           onPress={() => this.barTapped(2)}>
           <Text
             style={[
               styles.barText,
-              this.state.index == 2
-                ? { color: '#F818D9' }
-                : { color: 'black' },
+              this.state.index === 2 ? {color: '#F818D9'} : {color: 'black'},
             ]}>
             ATTENDING EVENTS
           </Text>
         </TouchableOpacity>
       </View>
-    )
-  }
+    );
+  };
 
   // Empty Component
   emptyListComponent = () => {
-    return(
-      <View style={{
-        flex:1,
-        alignItems:'center',
-        alignSelf:'center',
-        justifyContent:'center',
-        flexGrow:1,
-        display:'flex',
-        marginTop: SCREEN.height / 4
-      }}>
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          alignSelf: 'center',
+          justifyContent: 'center',
+          flexGrow: 1,
+          display: 'flex',
+          marginTop: SCREEN.height / 4,
+        }}>
         {this.state.index === 1 && (
-          <View >
-            <Text style={{textAlign:'center',marginBottom:20}}>
-            You are not hosting any events at the moment.
-            </Text>
+          <View>
+            <Text style={styles.emptyFont}>You are not hosting any events at the moment.</Text>
             <TouchableOpacity style={styles.btnMap}>
               <Text style={styles.btnText}>HOST?</Text>
             </TouchableOpacity>
           </View>
         )}
-        {this.state.index == 2 && (
+        {this.state.index === 2 && (
           <View>
             <Text style={styles.emptyFont}>
               You are not attending any events at the moment.
@@ -202,14 +214,13 @@ import Loader from '../../component/Loader';
           </View>
         )}
       </View>
-    )
-  }
+    );
+  };
   render() {
     return (
       <View style={styles.wrapperView}>
         <SafeAreaView style={styles.contentView}>
           <HeaderWithOptionBtn
-
             borderBottom={true}
             backColor={WHITE.dark}
             leftPress={() => this.props.navigation.openDrawer()}
@@ -217,7 +228,6 @@ import Loader from '../../component/Loader';
             rightPress={() => this.props.navigation.navigate('createEvent')}
             rightIcon={require('../../assets/plus.png')}
             headerTitle={'Manage Events'}
-
           />
 
           {this.topBar()}
@@ -233,34 +243,44 @@ import Loader from '../../component/Loader';
               data={this.state.userEvents}
               keyExtractor={item => item.id}
               ListEmptyComponent={this.emptyListComponent}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("myEventInfo",{id:item.id})}
+              renderItem={({item}) => (
+                <TouchableOpacity
+                  onPress={() =>
+                    this.props.navigation.navigate('myEventInfo', {id: item.id})
+                  }
                   style={{
                     borderBottomWidth: 1,
                     borderBottomColor: 'lightgrey',
                   }}>
                   <View style={styles.flexRow}>
                     <View style={styles.imgView}>
-
-                      <Image source={{uri:item.image}} style={{ borderRadius: 44, height: 60, width: 60 }} />
+                      <Image
+                        source={require('../../assets/image2.jpg')}
+                        style={{borderRadius: 44, height: 60, width: 60}}
+                      />
 
                       <Image
-                        style={{ position: 'absolute', right: -10 }}
+                        style={{position: 'absolute', right: -10}}
                         source={require('../../assets/private.png')}
                       />
                     </View>
 
                     <View style={styles.detail}>
                       <Text style={styles.titleText}>{item.Name}</Text>
-                      <Text style={styles.subtitleText}>{item.EventType === 'SCAN' ? 'SCAN-&-PAY AT DOOR' : item.EventType}</Text>
-                      <Text style={[styles.purpleText, { marginTop: 5 }]}>{moment(item.DateTime).format('hh:mm A | MMM DD, YYYY - ddd')}</Text>
+                      <Text style={styles.subtitleText}>
+                        {item.EventType === 'SCAN'
+                          ? 'SCAN-&-PAY AT DOOR'
+                          : item.EventType}
+                      </Text>
+                      <Text style={[styles.purpleText, {marginTop: 5}]}>
+                        {item.DateTime}       
+                      </Text>
                     </View>
                     <TouchableOpacity
-                      //  onPress={()=>this.props.navigation.navigate("directInvites")} 
+                      //  onPress={()=>this.props.navigation.navigate("directInvites")}
                       style={styles.shareView}>
                       <Image source={require('../../assets/share.png')} />
                     </TouchableOpacity>
-
                   </View>
                 </TouchableOpacity>
               )}
@@ -270,44 +290,53 @@ import Loader from '../../component/Loader';
             <FlatList
               data={this.state.userAttendedEvents}
               keyExtractor={item => item.id}
-              // ListEmptyComponent={this.emptyListComponent}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("attendingEventInfo",{id:item.Event.id})}
+              ListEmptyComponent={this.emptyListComponent}
+              renderItem={({item}) => (
+                <TouchableOpacity
+                  onPress={() =>
+                    this.props.navigation.navigate('attendingEventInfo', {
+                      id: item.Event.id,
+                    })
+                  }
                   style={{
                     borderBottomWidth: 1,
                     borderBottomColor: 'lightgrey',
                   }}>
                   <View style={styles.flexRow}>
                     <View style={styles.imgView}>
-
-                    <Image source={{uri:item.Event.image}} style={{ borderRadius: 44, height: 60, width: 60 }} />
+                      <Image
+                        source={require('../../assets/image2.jpg')}
+                        style={{borderRadius: 44, height: 60, width: 60}}
+                      />
 
                       <Image
-                        style={{ position: 'absolute', right: -10 }}
+                        style={{position: 'absolute', right: -10}}
                         source={require('../../assets/private.png')}
                       />
                     </View>
 
                     <View style={styles.detail}>
-                    <Text style={styles.titleText}>{item.Event.Name}</Text>
-                      <Text style={styles.subtitleText}>Host: {item.Event.Host.displayName}</Text>
-                      <Text style={[styles.purpleText, { marginTop: 5 }]}>{moment(item.Event.datetime).format('hh:mm A | MMM DD, YYYY - ddd')}</Text>
+                      <Text style={styles.titleText}>{item.Event.Name}</Text>
+                      <Text style={styles.subtitleText}>
+                        Host: {item.Event.Host.displayName}
+                      </Text>
+                      <Text style={[styles.purpleText, {marginTop: 5}]}>
+                        {item.DateTime
+                        }
+                      </Text>
                     </View>
                     <TouchableOpacity
-                      // onPress={()=>this.props.navigation.navigate("directInvites")} 
+                      // onPress={()=>this.props.navigation.navigate("directInvites")}
                       style={styles.shareView}>
                       <Image source={require('../../assets/messageIcon.png')} />
                     </TouchableOpacity>
-
                   </View>
                 </TouchableOpacity>
               )}
             />
-
           )}
         </SafeAreaView>
         {this.state.loading && <Loader loading={this.state.loading} />}
-    
       </View>
     );
   }
@@ -320,8 +349,7 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-  };
+  return {};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(manageEvents);
 
@@ -332,51 +360,50 @@ const styles = StyleSheet.create({
     backgroundColor: WHITE.dark,
   },
   sharedView: {
-    width: SCREEN.width, height: 40, alignItems: 'center', backgroundColor: '#FF9500', flexDirection: 'row', justifyContent: 'space-between',
-
+    width: SCREEN.width,
+    height: 40,
+    alignItems: 'center',
+    backgroundColor: '#FF9500',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   contentView: {
     flex: 1,
 
     // width: SCREEN.width - 40,
     backgroundColor: WHITE.dark,
-  }, btnTextLocation: {
+  },
+  btnTextLocation: {
     fontSize: 16,
     color: 'white',
     textAlign: 'center',
     fontFamily: FONT.Nunito.regular,
-
   },
   btnText: {
     fontSize: 16,
-    color: 'white',
     textAlign: 'center',
     color: 'white',
     fontFamily: FONT.Nunito.regular,
   },
-  emptyFont:{
+  emptyFont: {
     fontSize: 20,
-    
-    color: '#494949',
     textAlign: 'center',
     color: '#494949',
     fontFamily: FONT.Nunito.regular,
-    marginBottom:20,
+    marginBottom: 20,
   },
   btnMap: {
-      
     borderRadius: 25,
-     height: 50,
-     alignSelf:'center',
-     marginBottom:20,
-     width:SCREEN.width-40,
-     backgroundColor: 'black',
-     justifyContent: 'center'
-   },
+    height: 50,
+    alignSelf: 'center',
+    marginBottom: 20,
+    width: SCREEN.width - 40,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
   btnLocation: {
     width: wp('80%'),
     marginHorizontal: '10%',
-    borderRadius: 25,
     marginTop: hp('5%'),
     height: 50,
     elevation: 1,
@@ -389,8 +416,6 @@ const styles = StyleSheet.create({
   },
   flexRow: {
     flexDirection: 'row',
-
-
 
     alignItems: 'center',
   },
@@ -406,16 +431,15 @@ const styles = StyleSheet.create({
   detail: {
     width: wp('55%'),
     height: 80,
-    flexDirection:'column',
-    justifyContent:'center'
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
   imgView: {
     marginHorizontal: 20,
     alignItems: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   shareView: {
-
     width: wp('20%'),
     justifyContent: 'center',
   },
@@ -433,13 +457,11 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#F818D9',
 
-
     fontFamily: FONT.Nunito.bold,
   },
   barChild: {
     borderWidth: 1,
     width: wp('50%'),
-    height: 36,
     height: 40,
     borderColor: 'lightgrey',
     paddingTop: 12,
