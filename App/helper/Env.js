e00a0a8a0cc79b2e3e38dce74d8514d2a0065a41
Krutik Parikh <4krutikparikh@gmail.com>
2021-07-11 20:59:58 -0400

    Added Stripe

---
+++ App/helper/Env.js
@@ -0,0 +1,5 @@
+export const STRIPE_PUBLISHABLE_KEY = "sk_test_51GzwGrG6Y7lu9QzOXl5NhR0efutcpxamDYk5ES7tnyjFWzThLgIZodH3dS4wDEBHMBMM8vPjJpKz8W9P6SqKnlHl00Ga1d2j1P"
+export const APPLE_MERCHANT_IDENTIFIER = "merchant.Slizzr"
+
+//https://stripe.com/docs/payments/accept-a-payment?platform=react-native&ui=payment-sheet
+// Follow this one for both Apple and Google Pay. All things are setuped just implement UI and STUFF
